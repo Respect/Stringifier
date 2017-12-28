e9de43c8ea9a234a3afa8364abab6931b1de6d76
Henrique Moody <henriquemoody@gmail.com>
2017-12-28 19:29:10 +0100

    Create ObjectStringifier

---
+++ tests/unit/Stringifiers/ObjectStringifierTest.php
@@ -0,0 +1,83 @@
+<?php
+
+/*
+ * This file is part of Respect/Stringifier.
+ *
+ * (c) Henrique Moody <henriquemoody@gmail.com>
+ *
+ * For the full copyright and license information, please view the "LICENSE.md"
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Respect\Stringifier\Test\Stringifiers;
+
+use Respect\Stringifier\Quoter;
+use Respect\Stringifier\Stringifier;
+use Respect\Stringifier\Stringifiers\ObjectStringifier;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Respect\Stringifier\Stringifiers\ObjectStringifier
+ *
+ * @author Henrique Moody <henriquemoody@gmail.com>
+ */
+final class ObjectStringifierTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function shouldConvertToStringWhenRawValueIsAnObject(): void
+    {
+        $data = ['foo' => 1, 'bar' => false];
+
+        $raw = (object) $data;
+        $depth = 0;
+
+        $stringifiedData = '-stringified-';
+
+        $expectedValue = '[object] (stdClass: '.$stringifiedData.')';
+
+        $stringifierMock = $this->createMock(Stringifier::class);
+        $stringifierMock
+            ->expects($this->once())
+            ->method('stringify')
+            ->with($data, $depth + 1)
+            ->willReturn($stringifiedData);
+
+        $quoterMock = $this->createMock(Quoter::class);
+        $quoterMock
+            ->expects($this->once())
+            ->method('quote')
+            ->with($expectedValue, $depth)
+            ->willReturn($expectedValue);
+
+        $objectStringifier = new ObjectStringifier($stringifierMock, $quoterMock);
+
+        self::assertSame($expectedValue, $objectStringifier->stringify($raw, $depth));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldNotConvertToStringWhenRawValueIsNotTraversable(): void
+    {
+        $raw = true;
+        $depth = 123;
+
+        $stringifierMock = $this->createMock(Stringifier::class);
+        $stringifierMock
+            ->expects($this->never())
+            ->method('stringify');
+
+        $quoterMock = $this->createMock(Quoter::class);
+        $quoterMock
+            ->expects($this->never())
+            ->method('quote');
+
+        $objectStringifier = new ObjectStringifier($stringifierMock, $quoterMock);
+
+        self::assertNull($objectStringifier->stringify($raw, $depth));
+    }
+}
